a6f377516e89eefd94e4301234d42f86df1750ac
Kyle Ehlers <kce0002@auburn.edu>
2019-02-13 08:59:05 -0600

    Generated skeleton code for the video feature

---
+++ app/src/main/java/group6/interactivehandwriting/activities/Video/VideoMenuActivity.java
@@ -0,0 +1,15 @@
+package group6.interactivehandwriting.activities.Video;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import group6.interactivehandwriting.R;
+
+public class VideoMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.video_menu_layout);
+    }
+}
---
+++ app/src/main/java/group6/interactivehandwriting/activities/Video/VideoStreamActivity.java
@@ -0,0 +1,15 @@
+package group6.interactivehandwriting.activities.Video;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import group6.interactivehandwriting.R;
+
+public class VideoStreamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.video_stream_layout);
+    }
+}
