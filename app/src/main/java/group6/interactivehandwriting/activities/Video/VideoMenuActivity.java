e54b372a4e840ad8c72defce7dade189ce1c608e
Kyle Ehlers <kce0002@auburn.edu>
2019-02-13 09:31:50 -0600

    Linked video menu to video stream activity. Requested permission for camera on creation of stream activity.

--- app/src/main/java/group6/interactivehandwriting/activities/Video/VideoMenuActivity.java
+++ app/src/main/java/group6/interactivehandwriting/activities/Video/VideoMenuActivity.java
@@ -1,9 +1,14 @@
 package group6.interactivehandwriting.activities.Video;
 
+import android.Manifest;
+import android.content.Intent;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import group6.interactivehandwriting.R;
+import group6.interactivehandwriting.activities.Room.RoomActivity;
 
 public class VideoMenuActivity extends AppCompatActivity {
 
@@ -11,5 +16,10 @@ public class VideoMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.video_menu_layout);
+    }
+
+    public void startStream(View view) {
+        Intent stream_activity = new Intent(this, VideoStreamActivity.class);
+        VideoMenuActivity.this.startActivity(stream_activity);
     }
 }
